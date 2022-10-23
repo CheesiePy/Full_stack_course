b18be17cccff7bf382efc95fc46c1e9c273a4d76
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2022-10-23 22:21:30 +0300

    update

---
+++ lesson_13/script.js
@@ -0,0 +1,39 @@
+let human = "John Doe", gender = "Male", age = 25, height = 5.8, weight = 65, married = false, profession = "Engineer";
+console.log("Full Name: " + human);
+
+
+const parseon ={
+    name: "John Doe",
+    gender : "Male"
+};
+
+console.log(typeof(parseon));
+console.log(typeof(human));    
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+};
+
+
+// javascript arithmetic operators:
+/*
+    + addition
+    - subtraction
+    * multiplication
+    / division
+    % modulus
+    ** exponentiation
+    ++ increment
+    -- decrement
+*/
+
+// javascript assignment operators:
+/*
+    = assignment
+    += addition assignment
+    -= subtraction assignment
+    *= multiplication assignment
+    /= division assignment
+    %= modulus assignment
+    **= exponentiation assignment
+*/
---
+++ lesson_13/script_test.js
@@ -0,0 +1,31 @@
+
+// id_number = "203132999";
+// var id_number = "203132999";
+let fist_name = "John Doe", age = 25, height = 1.80, weight = 65, married = false, profession = "Engineer";
+
+let x;
+    
+x = 10;
+console.log(x);
+x = 20;
+console.log(x);
+x = 30;
+console.log(x);
+x++;
+console.log(x);    
+    
+x += 50;
+console.log(x);
+
+console.log("9" == 9);
+console.log("9" === 9); 
+
+
+let mayVariableIsGreat = "John Doe"; // camelCase
+
+let may_variable_is_great = "John Doe"; // snake_case
+
+
+console.log(typeof(mayVariableIsGreat));
+
+console.log(may_variable_is_great.split(" "));
