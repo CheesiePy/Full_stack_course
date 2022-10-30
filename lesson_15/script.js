d1858c3340b354cd8ee79dcdec15d6a714051a09
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2022-10-30 22:29:05 +0200

    update last script

--- lesson_15/script.js
+++ lesson_15/script.js
@@ -3,18 +3,76 @@
 
 
 
-const arr1 = [1, 2, 3, 5, 4];
-const arr2 = [1, "2", 3, "4", 5];
-const arr3 = [1, "2", 3, "4", 5, [1, 2, 3, 4, 5]];
+// const arr1 = [1, 2, 3, 5, 4];
 
-console.log(arr3.sort()); 
-//
+
+// const arr2 = [1, "2", 3, "4", 5];
+
+
+// const arr3 = [2, 22, 1, 1000, 1525];
+
+// const myArray = ["Python", "java", 2,"javascript", [1,[2,3],3]]
+
+// const element = myArray[4];
+
+// // console.log(myArray[0]);
+
+
+// console.log(arr3.sort()); 
+
+
+
+
+
 // sort() sorts the array in place and returns the lexical sorted array
 // use the compare function to sort the array numerically
 
-function compare(a, b) {
-    return a - b; // a - b returns a negative number if a < b 
-} 
+// function compare(a, b) {
+//     return a - b; // a - b returns a negative number if a < b 
+// } 
+// // + , 0 ,
+
+// console.log(arr3.sort(compare)); // sort the array numerically
 
 
-console.log(arr1.sort(compare)); // sort the array numerically
+
+
+// let x = "Galit";
+// console.log(x[0]);
+
+
+// function char_at(index, str){
+//     return str[index];
+// }
+// console.log(char_at(x, 0));
+
+function print_at(param1, param2, param3){
+    console.log(param3);
+    console.log(param1);
+    console.log(param2);
+    
+}
+
+const arr = ["x", "a"]
+
+let x = " hello world "
+
+// arr.pop();
+// console.log(arr)
+// arr.push("shigi")
+// console.log(arr)
+
+// print_at( "shalom", "samuel", "may");
+
+function greet(name_arr){
+    for(let i = 0; i < name_arr.length; i++){
+        console.log(`hello ${names[i]}`);
+    }
+}
+
+
+
+
+
+
+
