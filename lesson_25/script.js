516407408fdfa3c7be90ebb18cdfa9b46f9e0a6b
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2022-11-24 21:04:51 +0200

    update 24/11/22

--- lesson_25/script.js
+++ lesson_25/script.js
@@ -17,6 +17,7 @@ function reset() {
     for (let i = 0; i < buttonArray.length; i++) {
         buttonArray[i].textContent = "";
     }
+    counter = 0;
 }
 
 function ArrayToMatrix(arr, num) {
@@ -36,9 +37,50 @@ function ArrayToMatrix(arr, num) {
 }
 
  
-function checkForWin(arg) {
+function checkForWin(matrix, symbol) {
     // Check for a win
+    // return true;
+    let Rcounter, Ccounter, Acounter, TAcounter;
+    
+    for (let i = 0; i < matrix.length; i++){
+        Rcounter = 0;
+        Ccounter = 0;
+        Acounter = 0;
+        TAcounter = 0;
+
+        for(let j = 0; j < matrix[i].length; j++){
+
+            if(matrix[i][j].textContent == symbol){
+                // check for row
+                    Rcounter++;
+            }
+
+            if(matrix[j][i].textContent == symbol){
+                // check coliu
+                    Ccounter++;
+            }
+            if(matrix[j][j].textContent == symbol){
+                Acounter++;
+            }
+            if(matrix[j][matrix.length - j - 1].textContent == symbol){
+                TAcounter++;
+            }
+            
+        }
+        if(Rcounter == matrix.length || 
+            Ccounter == matrix.length ||
+            Acounter == matrix.length ||
+            TAcounter == matrix.length){
+            return true;
+        }
+    }
+    return false;
 }
+
+
+
+
+
 
 function checkforTie(arg) {
     // Check for a tie
@@ -47,7 +89,7 @@ function checkforTie(arg) {
 
 
 // Game Logic
-let conter = 0; // Keep track of the number of turns
+let counter = 0; // Keep track of the number of turns
 
 let buttonArray = document.querySelectorAll(".btn"); // Get all the buttons
 
@@ -56,7 +98,7 @@ buttonArray.forEach((button) => {
     coArray.push(button); // Push the buttons to the array
 });
 
-let matrix = ArrayToMatrix(coArray, 3); // Convert the array to a matrix
+let matrix = ArrayToMatrix(coArray, Math.sqrt(coArray)); // Convert the array to a matrix
 
 console.log(matrix);
 
@@ -65,17 +107,24 @@ for (let i = 0; i < buttonArray.length; i++) { // Loop through the buttons and a
 
     buttonArray[i].addEventListener("click", 
     function() {
-        if(conter % 2 == 0 && buttonArray[i].textContent == "") {
+        let symbol;
+        if(counter % 2 == 0 && buttonArray[i].textContent == "") {
             buttonArray[i].textContent = "X";
-            conter++;
-            // checkForWin(matrix); -> This is where we will check for a win
-        }else if(conter % 2 != 0 && buttonArray[i].textContent == "") {
+            counter++;     
+            symbol = 'X';
+        }else if(counter % 2 != 0 && buttonArray[i].textContent == "") {
             buttonArray[i].textContent = "O";
-            conter++;
-            // checkForWin(matrix); -> This is where we will check for a win
+            counter++;
+            symbol = 'O';
         }else{
             alert("This button is already clicked!");
         }
+
+        if(checkForWin(matrix, symbol)){
+            alert(`${symbol} has won`)
+            reset()
+        }
+        
     }
 );
 }
