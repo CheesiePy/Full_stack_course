1b6c20c7bde0dfbcdca3b0c1dfa6ce0a44932c87
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2022-11-29 21:41:22 +0200

    Homework 1 ! duo date 05/12/22 !

---
+++ lesson_27/homework.js
@@ -0,0 +1,123 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//    Home Assignment 1 - sorting algorithms 1 & string manipulation 1
+// ----------------------------------------------------------------------
+//              grading system: (for each question)
+//       
+// 1. if the code is not working - *0
+// 2. if the code is working but not according to the instructions - *0.5
+// 3. if there's no explanation -  *0.75
+// 4. late submission - final_grade -=  10*days_late
+//
+// ----------------------------------------------------------------------
+//  --------------- DUE DATE: 05/12/2022 at 11:59pm ---------------------
+// ______________________________________________________________________
+
+
+// ______________________________________________________________________
+//
+// ------------------------  PART 1 - 60 Points -------------------------
+//_______________________________________________________________________
+
+
+// 1. sorting algorithms - bubble sort, selection sort, insertion sort
+// part 1 - 60 points 20 points each
+
+/*
+* write the fulling functions below - you can't any built-in methods
+* click the link to see the algorithm
+* try implementing the algorithm in your own words
+*/
+
+// helpfull links:
+// link to visualize the sorting : https://visualgo.net/en/sorting
+// https://algorithm-visualizer.org/brute-force/bubble-sort (bubble sort)
+// https://algorithm-visualizer.org/brute-force/selection-sort (selection sort)
+// https://algorithm-visualizer.org/brute-force/insertion-sort (insertion sort)
+
+
+// 1.1 bubble sort - https://en.wikipedia.org/wiki/Bubble_sort
+const bubbleSort = (arr) => {};
+
+// 1.2 selection sort - https://en.wikipedia.org/wiki/Selection_sort
+
+const selectionSort = (arr) => {};
+
+// 1.3 insertion sort - https://en.wikipedia.org/wiki/Insertion_sort
+
+const insertionSort = (arr) => {};
+
+
+//_______________________________________________________________________
+//
+// ------------------------  PART 2 - 40 Points -------------------------
+//_______________________________________________________________________
+
+
+
+
+// 2. working with strings! - without using built-in methods!
+// part 2 - 40 points 10 points each
+
+
+// 2.1 Capitalize the first letter of a string
+const capitalize = (str) => {};
+
+// example:
+// let str = "hello world";
+// console.log(capitalize(str)); -> "Hello world"
+
+// 2.2 Capitalize the first letter of each word in a string
+const capitalizeWords = (str) => {};
+
+// example:
+// let str = "hey mom";
+// console.log(capitalizeWords(str)); -> "Hey Mom"
+  
+// 2.3 Reverse a string 
+const reverseString = (str) => {};
+// example:
+// let srt = "Pizza";
+// console.log(reverseString(str)); -> "azzip" - notice that the capital P is not capitalized
+
+
+// 2.4 trim a string - remove the white spaces from the beginning and end of a string
+// but not the white spaces in the middle of the string
+const trim = (str) => {};
+// example:
+// let str = "   hello world   ";
+// console.log(trim(str)); -> "hello world"
+
+
+//______________________________________________________________________
+//
+// ---------------------- BONUS QUESTIONS ------------------------------
+//______________________________________________________________________
+
+
+// Hard mode Bonus! - 15 points -> code wihout explanation will get 0 points
+
+// fibunacci sequence - https://en.wikipedia.org/wiki/Fibonacci_number
+// write a function that returns the nth number in the fibonacci sequence
+
+const fibonacci = (n) => {};
+
+// example:
+// let n = 7;
+// console.log(fibonacci(n)); -> 13
+
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// !submit your homework on github and send me the link to your homework!
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+// Good Luck!
+
+
+// #####################################################################
+//  ---------------------- Done? Submit! -------------------------------
+// #####################################################################
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
--- lesson_27/script.js
+++ lesson_27/script.js
@@ -2,27 +2,51 @@
 
 
 let mySum = (arr) => {
-    
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
 };
 
 // example mySum function:
 // let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // console.log(mySum(arr)); -> 55
 
-let myMax = (arr) => {};
+let myMax = (arr) => {
+    let max = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+};
 
 // example myMax function:
 // let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // console.log(myMax(arr)); -> 10
 
-let myMin = (arr) => {};
+let myMin = (arr) => {
+    let min = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    return min;
+};
 
 // example myMin function:
 // let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // console.log(myMin(arr)); -> 1
 
 
-let myAverage = (arr) => {};
+let myAverage = (arr) => {      
+    return mySum(arr) / arr.length;
+    // if you want to use the mySum function you have to write it above the myAverage function
+
+};
 // example myAverage function:
 // let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // console.log(myAverage(arr)); -> 5.5
@@ -31,7 +55,17 @@ let myAverage = (arr) => {};
 
 
 
-let myMedian = (arr) => {}; // median is the middle value of an array
+let myMedian = (arr) => {
+    let median = 0;
+    let sortedArr = mySort(arr); // if you want to use the mySort function you have to write it
+    if (sortedArr.length % 2 === 0) {
+        median = (sortedArr[sortedArr.length / 2 - 1] + sortedArr[sortedArr.length / 2]) / 2;
+    } else {
+        median = sortedArr[(sortedArr.length - 1) / 2];
+    }
+    return median;
+}; // median is the middle value of an array
+
 // example myMedian function:
 // let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // console.log(myMedian(arr)); -> 5.5
@@ -40,10 +74,52 @@ let myMedian = (arr) => {}; // median is the middle value of an array
 
 
 
-let myMode = (arr) => {}; // mode is the most common value of an array
+let myMode = (arr) => {
+    let mode = 0;
+    let maxCount = 0;
+    for (let i = 0; i < arr.length; i++) {
+        let count = 0;
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+            }
+        }
+        if (count > maxCount) {
+            maxCount = count;
+            mode = arr[i];
+        }
+    }
+    return mode;
 
-let myRange = (arr) => {}; // range is the difference between the largest and smallest values of an array
+}; // mode is the most common value of an array
 
-let myVariance = (arr) => {}; // variance is the average of the squared differences from the Mean
+let myRange = (arr) => {
+    let range = myMax(arr) - myMin(arr);
+    return range;
+}; // range is the difference between the largest and smallest values of an array
 
-let mySort = (arr) => {}; // sort an array from smallest to largest 
+let myVariance = (arr) => {
+    let variance = 0;
+    let average = myAverage(arr);
+    for (let i = 0; i < arr.length; i++) {
+        variance += (arr[i] - average) ** 2;
+    }
+    variance = variance / arr.length;
+    return variance;
+}; // variance is the average of the squared differences from the Mean
+
+
+let mySort = (arr) => { // sort an array from smallest to largest
+    let sortedArr = [];
+    let min = myMin(arr);
+    let max = myMax(arr);
+    for (let i = min; i <= max; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] == i) {
+                sortedArr.push(arr[j]);
+            }
+        }
+    }
+    return sortedArr;
+    // this is a very bad way to sort an array, but it works! 
+}; // sort an array from smallest to largest 
