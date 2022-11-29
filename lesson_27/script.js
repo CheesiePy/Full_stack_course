8f3b9b7d55f14232f7e03f720b3dd9e207b1f02e
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2022-11-29 18:56:24 +0200

    update 29/11/22

---
+++ lesson_27/script.js
@@ -0,0 +1,49 @@
+// we are not allowed to use the built-in sort method, sum, max, min, average, median, mode, range, variance methods
+
+
+let mySum = (arr) => {
+    
+};
+
+// example mySum function:
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(mySum(arr)); -> 55
+
+let myMax = (arr) => {};
+
+// example myMax function:
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(myMax(arr)); -> 10
+
+let myMin = (arr) => {};
+
+// example myMin function:
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(myMin(arr)); -> 1
+
+
+let myAverage = (arr) => {};
+// example myAverage function:
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(myAverage(arr)); -> 5.5
+// let arr2 = [ 1,2,2,3,4,7,7,9]
+// console.log(myAverage(arr2)); -> 4.25
+
+
+
+let myMedian = (arr) => {}; // median is the middle value of an array
+// example myMedian function:
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(myMedian(arr)); -> 5.5
+// let arr2 = [ 1,2,2,3,4,7,7,9]
+// console.log(myMedian(arr2)); -> 3.5
+
+
+
+let myMode = (arr) => {}; // mode is the most common value of an array
+
+let myRange = (arr) => {}; // range is the difference between the largest and smallest values of an array
+
+let myVariance = (arr) => {}; // variance is the average of the squared differences from the Mean
+
+let mySort = (arr) => {}; // sort an array from smallest to largest 
