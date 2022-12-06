1883d1e822e0b5b081d0a9c851aa8c0d91af973f
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2022-12-06 22:05:19 +0200

    update 06/12/22

--- lesson_28/src/script.js
+++ lesson_28/src/script.js
@@ -1,3 +1,21 @@
-function red(link) {
+let Data_Base;
+
+
+let json = fetch('../data_base.json')
+            .then(r => r.json())
+            .then(data => {
+                Data_Base = data;
+            });
+
+
+
+
+
+let showUser = () =>{
+    alert("username: " + Data_Base.test_user.username + " password: " + Data_Base.test_user.password);
+}
+
+
+function redirect(link) {
     location.assign(link);
- }
+}
