604671949a24d99b34df8755961ab8c169aeb161
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2022-12-13 17:39:03 +0200

    13/12/22

--- lesson_29/async.js
+++ lesson_29/async.js
@@ -1,23 +1,54 @@
 // Async Functions
-function resolveAfter2Seconds() {
-    return new Promise(resolve => {
-        setTimeout(() => {
-        resolve('resolved');
-        }, 2000);
-    });
-  }
+// function resolveAfter2Seconds() {
+//     return new Promise(resolve => {
+//         setTimeout(() => {
+//         resolve('resolved');
+//         }, 2000);
+//     });
+//   }
   
-  async function asyncCall() {
-    console.log('calling');
-    const result = await resolveAfter2Seconds();
-    console.log(result);
-    for (let i = 0; i < 10; i++) {
-        console.log(i);
-      }
-    // expected output: "resolved"
-  }
+//   async function asyncCall() {
+//     console.log('calling');
+//     const result = await resolveAfter2Seconds();
+//     console.log(result);
+//     for (let i = 0; i < 10; i++) {
+//         console.log(i);
+//       }
+//     // expected output: "resolved"
+//   }
   
-  asyncCall();
+let asyncForLoop = async (stop,time=2000,start=0,jump=1) => {
 
-  
-  
+    for (let i = start; i < stop; i+=jump) {
+    console.log("hey");
+        await new Promise((resolve, reject) => {
+            setTimeout(() => {    
+                resolve(i);
+            }, time);
+        }).then(
+            r => { 
+                console.log(r)
+            })
+    }
+}
+
+
+let asyncWhile = async (stop,time=2000,start=0,jump=1) => {
+    let i = start;
+    while (i < stop) {
+    console.log("hey");
+        await new Promise((resolve, reject) => {
+            setTimeout(() => {    
+                resolve(i+=jump);
+            }, time);
+        }).then(
+            r => { 
+                console.log(r)
+            })
+    }
+}
+
+
+
+let f = await asyncForLoop(10, 1000);
+asyncWhile(10, 1000);
--- lesson_29/promise.js
+++ lesson_29/promise.js
@@ -2,49 +2,46 @@
 
 // Promise is an object that represents the eventual completion or failure of an asynchronous operation
 
-const a = 3;
-let promise = new Promise((resolve, reject) => {
-    if (a == 3) {
-        resolve("success");
-    }
-    else {
-        reject("failed");
-    }
-}).then(result => {
-    console.log(result)
-    console.log(c.number);
-}).catch(error => {
-    console.log(error);
-});
+// const a = 3;
+// let promise = new Promise((resolve, reject) => {
+//     if (a == 3) {
+//         resolve("success");
+//     }
+//     else {
+//         reject("failed");
+//     }
+// }).then(result => {
+//     console.log(result)
+//     console.log(c.number);
+// }).catch(error => {
+//     console.log(error);
+// });
 
-let c = new Object();
-c.number = 5;
-
-
-if (c.number == 10) {
-    console.log("true");
-} else {
-    console.log("false");
-}
+// let c = new Object();
+// c.number = 5;
 
 
+// if (c.number == 10) {
+//     console.log("true");
+// } else {
+//     console.log("false");
+// }
 
-// // async
-// setTimeout(() => {
-//     console.log("hey");
-// }, 3000);
 
-// setTimeout(() => {
-//     setTimeout(() => {
-//         console.log("hey");
-//     }, 0);
 
-// }, 2000);
+// async
+setTimeout(() => {
+    console.log("hey1");
+}, 2000);
 
-// setTimeout(() => {
-//     c.number = 10;
-//         console.log(c);
-// }, 1000);
+setTimeout(() => {
+    console.log("hey3");
+}, 2000);
+
+setTimeout(() => {
+    console.log("hey2");
+}, 2000);
+
 
 
 
