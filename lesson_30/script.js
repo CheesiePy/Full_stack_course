adcd00afd8e84f95ffd53058eb4b274dfed0a614
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2022-12-13 18:49:57 +0200

    update 13/12/2022

---
+++ lesson_30/script.js
@@ -0,0 +1,39 @@
+// Pratice 2 - Fill in the empty functions 
+
+
+// Q1: instanceOf counts the number of instances of e in a collection
+let instanceOf = (element, collection) => {}
+
+//instanceOf(1, [1, 2, 3, 1, 1, 4]) // 3
+//instanceOf("a", "banana") // 3
+//instanceOf(5, {a: 5, b: 5}) // 2
+
+
+// Q2: 
+    // 1.
+    //count the number of users in "data_base_l1.json" 
+    //and return a list of their names
+
+    // 2.
+    // in "data_base_l2.json" and return a list of lists with size 2  -> [[name, age], [name, age], ...]
+    // each list contains the name and the age of the user
+
+    // 3.
+    // in "data_base_l3.json"
+    // return a list of emails that are created from name+_+last_name + "@gmail.com" 
+    // for example: "john doe" -> john_doe@gmail.com
+
+
+// Q3:
+    // 1. create a login page (you can reuse the code from the previous lesson)
+    // 2. assume that the user is logged in if the user name is "admin" and the password is "1234"
+    // 3. if the user is logged in, show a welcome message and table of all users in "data_base_l3.json" and their emails
+    
+
+
+
+ 
+
+
+
+     
