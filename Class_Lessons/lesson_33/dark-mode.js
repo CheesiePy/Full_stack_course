b66992f2079fe4005dcf14c9f5499cd2ee4a8d07
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2022-12-27 18:43:00 +0200

    update 27/12/22

---
+++ Class_Lessons/lesson_33/dark-mode.js
@@ -0,0 +1,2 @@
+// build a js script using JQuery to add a toggle botton that changes any page to dark-mode when pressed.
+// try your "dark-mode.js" on index1.html, index2.html and index3.html without changing the html files!
