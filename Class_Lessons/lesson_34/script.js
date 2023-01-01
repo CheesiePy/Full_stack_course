2628b9f0f882a8af74e694d67423359e3779f525
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2023-01-01 19:27:23 +0200

    update 01/01/2023

---
+++ Class_Lessons/lesson_34/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $('#box').click(function() {
+        $(this).animate({
+            right: '+=500px',
+        }, 1000, function() {
+          $(this).animate({top: '+=250px'}, 1000).fadeOut(1000); // we can chain animations!
+        });
+      });
+    }); 
+
+
+$(document).ready(function() {
+        $("#btn-display").click(function() {
+            $("#box2").fadeToggle("fast");
+            $("#box").css("background-color", "#ff0000");
+        })});
+
+
+
+
+// "fliping cards" animation without animate
+$(document).ready(function() { 
+  $('#card').click(function() {
+    $(this).toggleClass('flip');
+  });
+});
+
+// "fliping cards" animation with animate
